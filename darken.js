de1bd4581af2dbca99eb5638577b51138b66e2e0
Naveen Bobbala <career.naveenr@gmail.com>
2018-09-02 09:21:11 -0500

    Use Javascript AirBnb Style

--- darken.js
+++ darken.js
@@ -1,5 +1,6 @@
 (() => {
 	let styles="* { background: #002b36 ! important; color: #586e75 !important } :link, :link * { color: #3772D7 !important } :visited, :visited * { color: #7A49C7 !important }";
-	if(document.createStyleSheet)
+	if(document.createStyleSheet) {
 		document.createStyleSheet(`javascript:"${styles}"`);
+	}
 })();
