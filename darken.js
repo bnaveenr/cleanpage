7053593883737388e5f86f042a59c465f158f0dd
Naveen Bobbala <career.naveenr@gmail.com>
2018-09-02 09:12:00 -0500

    Check If Browser Supports document.createStyleSheet

--- darken.js
+++ darken.js
@@ -1,4 +1,5 @@
 (() => {
 	let styles="* { background: #002b36 ! important; color: #586e75 !important } :link, :link * { color: #3772D7 !important } :visited, :visited * { color: #7A49C7 !important }";
-	document.createStyleSheet(`javascript:"${styles}"`);
+	if(document.createStyleSheet)
+		document.createStyleSheet(`javascript:"${styles}"`);
 })();
